fd26fb7d6c69d1ac8055130dd8185f725452a5e7
Janetta Kataoeva <jkattoeva@gmail.com>
2022-02-27 21:44:23 +0600

    styled articles

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 const articles = [
   {
-    id: "article-1",
-    title: "Hello world",
-    image: "./butterfly.jpeg",
-    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates earum ducimus et laboriosam quae dolore iure recusandae? Quae cumque corrupti eum ipsam nostrum, quasi recusandae maxime deserunt vitae, eveniet perferendis.",
+    id: "article1",
+    title: "China",
+    image: "./china.jpg",
+    body: "China (Chinese: 中国; pinyin: Zhōngguó), officially the People's Republic of China (PRC; Chinese: 中华人民共和国; pinyin: Zhōnghuá Rénmín Gònghéguó), is a country in East Asia. It is the world's most populous country, with a population of more than 1.4 billion. China spans five geographical time zones and borders 14 different countries, the second most of any country in the world after Russia. Covering an area of approximately 9.6 million square kilometers (3,700,000 sq mi), it is the world's third or fourth largest country.[i] The country consists of 23 provinces,[j] five autonomous regions, four municipalities, and two Special Administrative Regions (Hong Kong and Macau). The national capital is Beijing.",
   },
   {
-    id: "article-2",
-    title: "Hello world",
-    image: "./butterfly.jpeg",
-    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates earum ducimus et laboriosam quae dolore iure recusandae? Quae cumque corrupti eum ipsam nostrum, quasi recusandae maxime deserunt vitae, eveniet perferendis.",
+    id: "article2",
+    title: "USA",
+    image: "./usa.jpg",
+    body: "The United States of America (U.S.A. or USA), commonly known as the United States (U.S. or US) or America, is a country primarily located in North America. It consists of 50 states, a federal district, five major unincorporated territories, 326 Indian reservations, and nine minor outlying islands.[h] At nearly 3.8 million square miles (9.8 million square kilometers), it is the world's third- or fourth-largest country by geographic area.[c] The United States shares land borders with Canada to the north and Mexico to the south as well as maritime borders with the Bahamas, Cuba, Russia, and other countries.[i] With a population of more than 331 million people,[j] it is the third-most-populous country in the world. The national capital is Washington, D.C., and the most populous city and financial center is New York City.",
   },
   {
-    id: "article-3",
-    title: "Hello world",
-    image: "./butterfly.jpeg",
-    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates earum ducimus et laboriosam quae dolore iure recusandae? Quae cumque corrupti eum ipsam nostrum, quasi recusandae maxime deserunt vitae, eveniet perferendis.",
+    id: "article3",
+    title: "Japan",
+    image: "./japan.jpg",
+    body: "Japan (Japanese: 日本, Nippon or Nihon,[nb 1] and formally 日本国)[nb 2] is an island country in East Asia. It is situated in the northwest Pacific Ocean, and is bordered on the west by the Sea of Japan, while extending from the Sea of Okhotsk in the north toward the East China Sea and Taiwan in the south. Japan is a part of the Ring of Fire, and spans an archipelago of 6852 islands covering 377,975 square kilometers (145,937 sq mi); the five main islands are Hokkaido, Honshu (the mainland), Shikoku, Kyushu, and Okinawa. Tokyo is the nation's capital and largest city; other major cities include Yokohama, Osaka, Nagoya, Sapporo, Fukuoka, Kobe, and Kyoto.",
   }
 ];
 
